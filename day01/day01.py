a59e0235a7c07fabfc7cb50558dca7e694f28592
Ivan Bratović <ibratovic@neogov.net>
2022-12-30 11:07:03 +0100

    add day01 solution

---
+++ day01/day01.py
@@ -0,0 +1,27 @@
+test = False
+
+def read_input():
+    global test
+    filename = "input.txt"
+    if test:
+        filename = "test.txt"
+    with open(filename, "r") as file:
+        return file.readlines()
+
+def get_calories():
+    calories_by_elf = [0]
+    idx = 0
+    for line in read_input():
+        try:
+            calories_by_elf[idx] += int(line)
+        except ValueError:
+            idx += 1
+            calories_by_elf.append(0)
+    return calories_by_elf
+
+def main():
+    print(max(get_calories()))
+    print(sum(sorted(get_calories(), reverse=True)[:3]))
+
+if __name__ == "__main__":
+    main()
